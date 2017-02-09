0287503dec9ee210dd37eee080f411033e9ff33b
Muthu Palaniappan <amuthu1996@gmail.com>
2017-02-09 21:46:49 +0530

    add MongoDB

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import pymongo
+from pymongo import MongoClient
+
+
+MONGO_URL = mongodb://muthu:qwerty1234@ds147979.mlab.com:47979/
+client = MongoClient(MONGO_URL)
+
+# Specify the database
+db = client.locationtrack
+collection = db.shoutouts
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/", methods=['GET'])
+def index():
+    shouts = collection.find()
+    return render_template('index.html', shouts=shouts)
+
+@app.route("/post", methods=['POST'])
+def post():
+    shout = {"locations":request.form['locations']}
+    shout_id = collection.insert(shout)
+    return redirect('/')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
